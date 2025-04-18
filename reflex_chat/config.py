90a2d82dd90e31fcd446142fb34c37c570d31064
supmo668 <mymm.psu@gmail.com>
2025-04-17 23:45:27 -0700

    first commit

--- reflex_chat/config.py
+++
@@ -1,5 +0,0 @@
-import logging
-BOTTOM_ELEMENT_ID = "chat-bottom"
-LOG_LEVEL = logging.DEBUG
-
-logging.basicConfig(level=LOG_LEVEL)
--- reflex_chat/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility modules for the reflex-chat application."""
-
-from .token_manager import token_manager
-
-__all__ = ["token_manager"]
